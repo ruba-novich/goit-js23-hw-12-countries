cb6bb6744c446cd263bba8a4cd8d3e20f237b542
ruba-novich <rubanovichartem@gmail.com>
2020-11-30 22:07:37 +0200

    1

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import './styles.css';
+
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+import { error } from '@pnotify/core';
+
+import countryList from './templates/country-list.hbs';
+import countryCard from './templates/country-card.hbs';
+import fetchCountries from './js/fetchCountries';
+
+const debounce = require('lodash.debounce');
+
+const refs = {
+  input: document.querySelector('.input'),
+  output: document.querySelector('.result'),
+};
+
+refs.input.addEventListener('input', debounce(onSearchInput, 500));
+
+function onSearchInput(e) {
+  if (!e.target.value) {
+    refs.output.innerHTML = '';
+    return;
+  }
+  fetchCountries(e.target.value)
+    .then(countries => {
+      if (countries.status === 404) {
+        return Promise.reject(
+          'The country for your request was not found.Please try again',
+        );
+      }
+
+      if (countries.length > 10) {
+        error({
+          text: 'Too many matches found.Please enter a more specific query!',
+        });
+        return;
+      }
+
+      countries.length >= 2
+        ? renderCountriesList(countries)
+        : renderCountryCard(countries);
+    })
+    .catch(err => {
+      error({
+        text: err,
+      });
+    });
+}
+
+function renderCountriesList(countries) {
+  refs.output.innerHTML = countryList(countries);
+}
+function renderCountryCard(countries) {
+  refs.output.innerHTML = countryCard(countries);
+}
